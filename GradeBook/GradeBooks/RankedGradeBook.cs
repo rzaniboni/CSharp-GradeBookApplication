da00375271b9f960b4ee01df24bd75423ca36941
Roberto Zaniboni <zaniboni@gmail.com>
2020-06-27 18:35:15 +0200

    3 fix GetGradeLetter

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks {
@@ -13,19 +14,25 @@ namespace GradeBook.GradeBooks {
     public override char GetLetterGrade (double averageGrade) {
 
       if (Students.Count <= 5) {
-        throw new InvalidOperationException();
+        throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
       }
 
-      if (averageGrade >= 80)
+      var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+      var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+      if (grades[threshold -1] <= averageGrade) {
         return 'A';
-      else if (averageGrade >= 60)
-        return 'B';
-      else if (averageGrade >= 40)
-        return 'C';
-      else if (averageGrade >= 20)
-        return 'D';
-      else
-        return 'F';
+      } else if (grades[(threshold *2) -1] <= averageGrade) {
+        return  'B';
+      } else if (grades[(threshold *3) -1] <= averageGrade) {
+        return  'C';
+      } else if (grades[(threshold *4) -1] <= averageGrade) {
+        return  'D';
+      } else if (grades[(threshold *5) -1] <= averageGrade) {
+        return  'E';
+      }
+
+      return base.GetLetterGrade(averageGrade);
     }
 
   }
