de70d23160a7bc246e9a856ae58083b13bf6dec0
Roberto Zaniboni <zaniboni@gmail.com>
2020-06-27 18:11:24 +0200

    2 Creating New GradeBookTypes

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -12,6 +12,7 @@ namespace GradeBook.GradeBooks
     public class BaseGradeBook
     {
         public string Name { get; set; }
+        public GradeBookType Type { get; set; }
         public List<Student> Students { get; set; }
 
         public BaseGradeBook(string name)
@@ -263,7 +264,7 @@ namespace GradeBook.GradeBooks
                              from type in assembly.GetTypes()
                              where type.FullName == "GradeBook.GradeBooks.StandardGradeBook"
                              select type).FirstOrDefault();
-            
+
             return JsonConvert.DeserializeObject(json, gradebook);
         }
     }
